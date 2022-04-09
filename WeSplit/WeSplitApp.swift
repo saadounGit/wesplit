08df4a4f51167f703f563d4c1ad498849fdd355d
Saadoun Alerris <saadoun@protonmail.com>
2022-04-09 21:02:23 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Saadoun Alerris on 09/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
